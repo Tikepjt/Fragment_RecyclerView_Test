1f0b633968cd8517d716049c1e5cff86cf43d002
tikepjt <Tikepjt@gmail.com>
2018-08-14 18:01:31 +0900

    first commit

---
+++ app/src/main/java/com/example/summit/fragment_recyclerview_test/Friends/FriendsActivity.java
@@ -0,0 +1,17 @@
+
+package com.example.summit.fragment_recyclerview_test.Friends;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.summit.fragment_recyclerview_test.R;
+
+public class FriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+
+    }
+}
